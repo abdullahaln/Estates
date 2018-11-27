f995802ce4333ee495d7a8d51dc231d55450c8d6
Semos5245 <s.alan.abd1998@gmail.com>
2018-11-27 14:14:03 +0200

    Finished the Messages and the Tags controllers

--- Estates/Controllers/MessagesController.cs
+++ Estates/Controllers/MessagesController.cs
@@ -11,6 +11,9 @@ namespace Estates.Controllers
     [RoutePrefix("api/Messages")]
     public class MessagesController : ApiController
     {
+
+        #region GET
+
         ApplicationDbContext db = new ApplicationDbContext();
 
         //GET: api/Messages/GetAllMessages    
@@ -33,25 +36,95 @@ namespace Estates.Controllers
         [Route("GetMessages")]
         public IHttpActionResult GetMessages(string fromid,string toid)
         {
-            var messages = db.Messages.Where(i => (i.FromId == fromid && i.ToId == toid) && (i.ToId == fromid && i.FromId == toid));
+            var messages = db.Messages.Where(i => (i.FromId == fromid && i.ToId == toid) && (i.ToId == fromid && i.FromId == toid)).ToList();
 
             if (messages == null)
                 return NotFound();
 
             return Ok(new
             {
-                message = "Messages have recived successfully",
-                totalResult = messages.Count(),
-                result = messages
+                Message = "Messages have recived successfully",
+                ResultsCount = messages.Count,
+                Result = messages
             });
         }
 
+        #endregion
+
+        #region POST
+
         //POST: api/Messages/SendMessage
+        //Sends a message from a person to a person
         [Route("SendMessage")]
         [HttpPost]
         public IHttpActionResult SendMessage(MessageViewModel model)
         {
-            return Ok();
+            if (ModelState.IsValid)
+            {
+                var sender = db.People.Find(model.FromId);
+
+                var receiver = db.People.Find(model.ToId);
+
+                if (sender == null || receiver == null)
+                {
+                    return BadRequest("Sender or Receiver id is invalid");
+                }
+
+                var message = new Message
+                {
+                    MessageId = Guid.NewGuid().ToString(),
+                    MessageDate = DateTime.UtcNow,
+                    FromId = model.FromId,
+                    ToId = model.ToId,
+                    Description = model.Description.Trim()
+                };
+
+                db.Messages.Add(message);
+                db.SaveChanges();
+
+                return Ok(new
+                {
+                    Message = "Message have been sent successfully",
+                    Status = "success",
+                    Result = message
+                });
+            }
+
+            return BadRequest("Some properties are not valid");
         }
+
+        #endregion
+
+        #region DELETE
+
+        //DELETE: api/Messages/DeleteMessage
+        //Deletes a specific message
+        [HttpDelete]
+        [Route("DeleteMessage")]
+        public IHttpActionResult DeleteMessage(string id)
+        {
+            if (String.IsNullOrEmpty(id))
+            {
+                return BadRequest("Invalid message id");
+            }
+
+            var message = db.Messages.Find(id);
+
+            if (message == null)
+            {
+                return NotFound();
+            }
+
+            db.Messages.Remove(message);
+            db.SaveChanges();
+
+            return Ok(new
+            {
+                Message = "Message has been deleted successfully",
+                Status = "success"
+            });
+        }
+
+        #endregion
     }
 }
--- Estates/Controllers/TagsController.cs
+++ Estates/Controllers/TagsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Estates.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,8 +8,95 @@ using System.Web.Http;
 
 namespace Estates.Controllers
 {
+    [RoutePrefix("api/Tags")]
     public class TagsController : ApiController
     {
+        ApplicationDbContext db = new ApplicationDbContext();
+
+        #region GET
+
+        //GET: api/Tags/GetAllTags
+        //Gets all the tags
+        [HttpGet]
+        [Route("GetAllTags")]
+        public IHttpActionResult GetAllTags()
+        {
+            var tags = db.Tags.ToList();
+
+            return Ok(new
+            {
+                Message = "Tags have been received successfully",
+                Status = "success",
+                ResultsCount = tags.Count,
+                Result = tags
+            });
+        }
+
+        //GET: api/Tags/GetTagsForEstate
+        //Gets all tags for a specific Estate by id
+        [HttpGet]
+        [Route("GetTagsForEstate")]
+        public IHttpActionResult GetTagsForEstate(string id)
+        {
+            if (String.IsNullOrEmpty(id))
+            {
+                return BadRequest("Invalid estate id");
+            }
+
+            var item = db.Items.Find(id);
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            var tags = db.Tags.Where(t => t.ItemId == id).ToList();
+
+            return Ok(new
+            {
+                Message = $"Tags for estate whose id is {item.ItemId} have been received successfully",
+                Status = "success",
+                ResultsCount = tags.Count,
+                Result = tags
+            });
+        }
+        #endregion
+
+        #region POST
+
+        //TODO:
+
+        #endregion
+
+        #region DELETE
+        //DELETE: api/Tags/DeleteTag
+        //Deletes a specific tag
+        [HttpDelete]
+        [Route("DeleteTag")]
+        public IHttpActionResult DeleteTag(string id)
+        {
+            if (String.IsNullOrEmpty(id))
+            {
+                return BadRequest("Invalid Tag Id");
+            }
+
+            var tag = db.Tags.Where(i => i.TagId == id) as Tag;
+
+            if (tag == null)
+            {
+                return NotFound();
+            }
+
+            db.Tags.Remove(tag);
+            db.SaveChanges();
+
+            return Ok(new
+            {
+                Message = "Tag has been deleted successfully",
+                Status = "success"
+            });
+        }
+        #endregion
 
     }
 }
